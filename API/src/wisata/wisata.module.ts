9af9360fa1931b5105ee633b892e9070fe59ae10
muhammadtammam-lab <muhammad_tammam@teknokrat.ac.id>
2026-05-30 09:28:07 +0700

    update fitur login admin(3)

--- API/src/wisata/wisata.module.ts
+++ API/src/wisata/wisata.module.ts
@@ -1,21 +1,19 @@
 import { Module } from '@nestjs/common';
-import { WisataController } from './wisata.controller';
-import { WisataService } from './wisata.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt'; // 1. Import JwtModule
+import { WisataService } from './wisata.service';
+import { WisataController } from './wisata.controller';
 import { Wisata } from './entities/wisata.entity';
 
-/**
- * WisataModule adalah wadah besar yang menyatukan 
- * database, pengatur rute, dan logika bisnis wisata.
- */
 @Module({
-  // 1. IMPORT: Menghubungkan modul ini ke tabel 'Wisata' di database (Wajib!)
-  imports: [TypeOrmModule.forFeature([Wisata])], 
-
-  // 2. CONTROLLERS: Menentukan siapa yang akan menerima tamu (request HTTP)
+  imports: [
+    TypeOrmModule.forFeature([Wisata]),
+    // 2. Daftarkan JwtModule dengan secret yang sama agar service-nya bisa dibaca oleh WisataGuard
+    JwtModule.register({
+      secret: 'KUNCI_RAHASIA_ADMIN',
+    }),
+  ],
   controllers: [WisataController],
-
-  // 3. PROVIDERS: Menentukan siapa yang akan mengerjakan tugas berat (logika data)
-  providers: [WisataService],
+  providers: [WisataService], // WisataGuard otomatis ikut terbaca karena dipakai di Controller
 })
 export class WisataModule {}
